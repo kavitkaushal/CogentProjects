a8f37e3bff27d5ca172c11a3a4e75987d008b0ab
Kavit <Kavit@Kavit>
2019-07-03 14:39:50 -0400

    Initial Commit

---
+++ E_Office/src/main/java/com/kavit/EOfficeApplication.java
@@ -0,0 +1,13 @@
+package com.kavit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EOfficeApplication.class, args);
+	}
+
+}
