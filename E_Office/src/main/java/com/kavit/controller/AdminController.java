f88335428aaf7740c44bf1efbe66c7a749ebf54c
Kavit <Kavit@Kavit>
2019-07-05 10:01:10 -0400

    Email sent upon new employee registration

--- E_Office/src/main/java/com/kavit/controller/AdminController.java
+++ E_Office/src/main/java/com/kavit/controller/AdminController.java
@@ -1,7 +1,12 @@
 package com.kavit.controller;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,6 +25,8 @@ public class AdminController {
 	private EmployeeRepo employee;
 	@Autowired
 	private DepartmentRepo department;
+	@Autowired
+	private JavaMailSender sender;
 	
 	@GetMapping("/")
 	String adminLogin() {
@@ -38,6 +45,19 @@ public class AdminController {
 	@PostMapping("/addEmp")
 	String add(@RequestBody Employee newEmp) {
 		employee.save(newEmp);
+		MimeMessage message=sender.createMimeMessage();
+		MimeMessageHelper helper=new MimeMessageHelper(message);
+		try {
+			helper.setTo("19kavit@gmail.com");
+			helper.setText("Welcome "+newEmp.getFirst_name()+" "+newEmp.getLast_name()+" to the e-Office management."
+							+"\n\n Your details are below. \n Employee ID: "+newEmp.getEmp_id()+"\n Employee Login: "+newEmp.getEmail_id()
+							+"\n\nRegards, \n Admin Team \n e-Office Management");
+			helper.setSubject("Welcome On-board "+newEmp.getFirst_name()+" "+newEmp.getLast_name());
+		} catch (MessagingException e) {
+			e.printStackTrace();
+			return "Exception while sending email";
+		}
+		sender.send(message);
 		return "New employee added";
 	}
 }
