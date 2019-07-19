ad816c347eb9670da65b4bf0250bd08400b180c9
Kavit <Kavit@Kavit>
2019-07-19 11:38:06 -0400

    Updated service package

--- E_Office/src/main/java/com/kavit/config/JwtRequestFilter.java
+++ E_Office/src/main/java/com/kavit/config/JwtRequestFilter.java
@@ -15,7 +15,7 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import com.saran.service.JwtUserDetailsService;
+import com.kavit.service.JwtUserDetailsService;
 
 import io.jsonwebtoken.ExpiredJwtException;
 
